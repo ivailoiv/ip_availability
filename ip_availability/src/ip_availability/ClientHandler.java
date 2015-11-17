bd9b6014cb160d70b151f9efa2745e37c9c57eaf
Ivailo Ivanov <ivailo0620@gmail.com>
2015-11-17 12:51:42 +0200

    Creating new repository + new project + ClientHandler

---
+++ ip_availability/src/ip_availability/ClientHandler.java
@@ -0,0 +1,48 @@
+package ip_availability;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable {
+	private static final String COMMAND_STOP_SERVER = "stopServer";
+
+	private final Socket socket;
+
+	private final EchoServer echoServer;
+
+	public ClientHandler(EchoServer echoServer, Socket socket) {
+		this.socket = socket;
+		this.echoServer = echoServer;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			final PrintStream out = new PrintStream(socket.getOutputStream());
+			final Scanner scanner = new Scanner(socket.getInputStream());
+			
+			while (scanner.hasNextLine()) {
+				final String line = scanner.nextLine();
+				if (COMMAND_STOP_SERVER.equals(line)) {
+					echoServer.stopServer();
+					break;
+				}
+				out.println(line);
+			}
+			scanner.close();
+			out.close();
+		} catch (IOException e) {
+			// TODO check if closed before printing error
+			e.printStackTrace();
+		} finally {
+			echoServer.onClientStopped(this);
+		}
+	}
+	
+	public void stopClient() throws IOException {
+		socket.close();
+		// TODO add variable closed
+	}
+}
